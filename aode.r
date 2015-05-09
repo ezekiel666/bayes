f881421e4b49b97821860868f9b6e02f853b3322
zerocool <c.pawlowski@stud.elka.pw.edu.pl>
2015-05-09 17:38:00 +0200

    aode training phase

--- aode.r
+++ aode.r
@@ -5,39 +5,80 @@ source("include.r")
 aode <- function(x, ...)
   UseMethod("aode") #generic function @see methods(aode)
 
+apply2 <- function(x, fun) {
+  list <- list()  
+  
+  for(i in colnames(x)) {
+    for(j in colnames(x)) {
+      list[[i]][[j]] <- fun(x[,i],x[,j])      
+    }
+  }  
+  return(list)
+}
+
+
 #training
 #x - attributes matrix
 #y - class vector
-#laplace - laplace smoothing coefficient (default-1)
+#laplace - laplace smoothing coefficient (default=1) @see http://en.wikipedia.org/wiki/Additive_smoothing
 #returns model(apriori - class frequencies, tables - attributes statistics, levels - class levels, call - method call)
 aode.default <- function(x, y, laplace = 1, ...) {
   call <- match.call() #text of method call
   Yname <- deparse(substitute(y)) #getting y name
   x <- as.data.frame(x)
   
-  #estimation-function
-  est <- function(var)
+  check <-function(var)
     if(is.numeric(var)) {
       stop("variable cannot be numeric - only discrete attributes are allowed")
-    } else {
-      tab <- table(y, var)
-      (tab + laplace) / (rowSums(tab) + laplace * nlevels(var))
-      #TODO: aode calculation instead of nb
     }
   
-  #create tables
-  apriori <- table(y) #count class frequencies
-  tables <- lapply(x, est)
+  lapply(x, check)
   
-  #fix dimname names
-  for(i in 1:length(tables)) {
-    names(dimnames(tables[[i]])) <- c(Yname, colnames(x)[i])
+  #estimation
+  est1 <- function(var) {
+    tab1 <- table(y, var) #count class frequencies for a given variable
+    (tab1 + laplace) / (rowSums(tab1) + laplace * nlevels(var))  
+    #print("tab1")
+    #print(tab1) #[y, var]
+    return(tab1)
   }
-  names(dimnames(apriori)) <- Yname
+  
+  est2 <- function(var1, var2) {  
+    vc <- table(var1, y) #count class frequencies for a given variable
+    #print("vc")
+    #print(vc) #[var1, y]
+    
+    tab2 <- table(var1, var2, y) #count variable pair frequency for a given class
+    #print("tab2 before")
+    #print(tab2) #[var1, var2, y]
+    
+    # for each tuple(var1,y) ...
+    for(f in dimnames(tab2)$var1){
+      for(s in dimnames(tab2)$y){
+        #print(paste("(",f,",",s,")", sep=""))
+        tab2[f,,s] <- (tab2[f,,s] + laplace) / (vc[f,s] + laplace * nlevels(var1))
+      }    
+    }
+    
+    #print("tab2 after")
+    #print(tab2)
+    return(tab2)
+  }
+  
+  cfreq <- table(y) #count class frequencies
+  names(dimnames(cfreq)) <- Yname 
+  
+  #create tables
+  tables1 <- lapply(x, est1)
+  #for(i in 1:length(tables1)) { # y -> yname
+  #  names(dimnames(tables1[[i]])) <- c(Yname, colnames(x)[i])
+  #}
+  tables2 <- apply2(x, est2)
   
   structure(
-    list(apriori = apriori,
-         tables = tables,
+    list(cfreq = cfreq,
+         tables1 = tables1,
+         tables2 = tables2,
          levels = levels(y),
          call   = call
     ),
@@ -48,8 +89,9 @@ aode.default <- function(x, y, laplace = 1, ...) {
 #predicting
 #model - model
 #vector - vector
-#returns predicted class for given vector
-aode.predict <- function(model, vector, ...) {
+#m - frequency lower limit (default=1)
+#returns predicted class for a given vector
+aode.predict <- function(model, vector, m=1, ...) {
   
 }
 
@@ -68,7 +110,8 @@ y <- dataTrain$spam
 
 #model<-aode(x,y) 
 
-a <- matrix(as.factor(seq(1:6)), nrow = 2, ncol = 3)
+a <- matrix(as.factor(1:6), nrow = 2, ncol = 3)
+colnames(a) <- c("first", "second", "thrid")
 b <- as.factor(c(0,1))
-  
-model<-aode(a,b) 
+
+model<-aode(a,b,1) 
