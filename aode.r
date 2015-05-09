8e13c41a2ad43d3819d6df60870953910ce590b3
zerocool <c.pawlowski@stud.elka.pw.edu.pl>
2015-05-09 11:32:39 +0200

    initial commit

---
+++ aode.r
@@ -0,0 +1,8 @@
+source("include.r")
+
+mydata <- readData(10)
+
+print(mydata)
+
+# listing output in console: mydata
+# clearing console: CTRL + E + L
---
+++ include.r
@@ -0,0 +1,21 @@
+require(caret)
+#install.packages("caret", dep = TRUE)
+
+readData <- function(s=1000) {
+  dataset <- read.csv("spambase/spambase.data",header=FALSE,sep=",")
+  names <- read.csv("spambase/names",header=FALSE,sep=",")
+  names(dataset) <- sapply((1:nrow(names)),function(i) toString(names[i,1]))
+  
+  #TODO: attribute discretization
+  #http://en.wikipedia.org/wiki/Discretization_of_continuous_features
+  
+  dataset$spam <- as.factor(dataset$spam)
+  sample <- dataset[sample(nrow(dataset), s),]
+  
+  #split 0.7/0.3 keeping $spam distribution
+  trainIndex <- createDataPartition(sample$spam, p = .7, list = FALSE, times = 1)
+  dataTrain <- sample[ trainIndex,]
+  dataTest  <- sample[-trainIndex,]
+  
+  return(list(dataTrain=dataTrain, dataTest=dataTest))
+}
