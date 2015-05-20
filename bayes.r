da34a0a4ac893be243b37ade3ec1101e6024d118
Michał Dębski <debski.mi.zd@gmail.com>
2015-05-20 22:20:18 +0200

    Added TAN for collecting predictions

--- bayes.r
+++ bayes.r
@@ -1,23 +1,38 @@
 source("include.r")
 source("aode.r")
+source("tan.r")
 
 require(e1071)
 #install.packages("e1071", dep = TRUE)
 
-data = readData();
+# Predicted table:
+#           \ real 0     1
+# predicted
+#       0         22     2
+#       1          1     8
+
+data = readData(10);
 dataTrain <- data$dataTrain
 dataTest <- data$dataTest
 
 x <- dataTrain[,-which(names(dataTrain) %in% c("spam"))]
 y <- dataTrain$spam
-model = naiveBayes(x,y)
 z <- dataTest[,-which(names(dataTest) %in% c("spam"))]
-predicted_class = predict(model, z, type = "class")
-predicted_raw = predict(model, z, type = "raw")
-comparsionNaive <- table(predicted_class, dataTest$spam)
 
-model = aode(x,y)
-z <- dataTest[,-which(names(dataTest) %in% c("spam"))]
-predicted_class = predict(model, z, type = "class")
-predicted_raw = predict(model, z, type = "raw")
-comparsionAODE <- table(predicted_class, dataTest$spam)
+modelNaive = naiveBayes(x,y)
+predictedClassNaive = predict(modelNaive, z, type = "class")
+# For probabilities
+#predictedRawNaive = predict(modelNaive, z, type = "raw")
+comparsionNaive <- table(predictedClassNaive, dataTest$spam)
+
+modelAODE = aode(x,y)
+predictedClassAODE = predict(modelAODE, z, type = "class")
+# For probabilities
+# predictedRawAODE = predict(modelAODE, z, type = "raw")
+comparsionAODE <- table(predictedClassAODE, dataTest$spam)
+
+modelTAN = tan(dataTrain)
+predictedClassTAN = predict(modelTAN, dataTest, type = "class")
+# For probabilities
+# predictedRawTAN = predict(modelTAN, dataTest, type = "raw")
+comparsionTAN <- table(predictedClassTAN, dataTest$spam)
